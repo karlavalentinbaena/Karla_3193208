5e78675fb07554dfbb855549fb6724798a3f901c
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2024-08-24 16:49:12 -0600

    app.js

---
+++ tareaUno/app.js
@@ -0,0 +1,5 @@
+let nombre = prompt('Hola, mi nombre es Karla, ¿Cúal es el tuyo? :');
+let edad = parseInt(prompt('Tengo 21 años y ¿tu?:'));
+
+console.log('El nombre ingresado es:', nombre);
+console.log('la edad es:', typeof edad);
