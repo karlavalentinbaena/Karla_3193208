7aaa076b7875ada7e2302337f4b481b57f79e141
Karla Valentin Baena <92067163+karlavalentinbaena@users.noreply.github.com>
2024-08-31 13:48:59 -0600

    Delete tareaTres directory

--- tareaTres/ejercicio.js
+++
@@ -1,8 +0,0 @@
-function suma(num1,num2){
-        return parseInt(num1)+parseInt(num2);
-}
-
-let prim = prompt("ingresa un número:");
-let segun = prompt("ingresa otro número:");
-let res = suma(prim,segun);
-console.log("La suma de los dos numeros dados es:"+res);
